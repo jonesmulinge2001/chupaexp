9b60d594c47f18fb111bf8ae2c0e856ba8ac7c4f
Jonathan Mulinge <jonathanmwandi@gmail.com>
2026-09-15 09:31:41 -0700

    Added serializers and model data validation

--- ChupaExp/chupaexpress/store/admin.py
+++ ChupaExp/chupaexpress/store/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'createdAt')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    list_editable = ('name',)
+    ordering = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'category', 'createdAt')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'description')
+    list_filter = ('category',)
+    editable_fields = ('price', 'category')
+    ordering = ('name')
--- ChupaExp/chupaexpress/store/serializers.py
+++ ChupaExp/chupaexpress/store/serializers.py
@@ -1,1 +1,17 @@
-# TODO
+from rest_framework import serializers
+from .models import Category, Product
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug', 'descrption', 'createdAt']
+        read_only_fields = ['id', 'slug', 'createdAt']
+
+class productSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'slug', 'description', 'price', 'category', 'createdAt']
+        read_only_fields = ['id', 'slug', 'createdAt']
+
